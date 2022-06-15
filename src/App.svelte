c6b6d6823c7f02da0b6a0b759a644ad1a9d53326
Divyansh Singh <divyanshanandsingh@gmail.com>
2022-06-16 02:03:10 +0530

    home nav

--- src/App.svelte
+++ src/App.svelte
@@ -18,8 +18,12 @@
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    margin: 0px;
+  }
+  :global(*) {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
   }
   nav {
     display: flex;
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,1 +1,22 @@
-home
+<script>
+</script>
+
+<div class="home-nav">
+  <p>All Products</p>
+  <p>Clothing</p>
+  <p>Toys</p>
+  <p>Books</p>
+  <p>Sports Equipment</p>
+</div>
+
+<style>
+  .home-nav {
+    display: flex;
+    justify-content: space-between;
+    background-color: #48a259;
+    color: white;
+    font-family: "Inter", sans-serif;
+    font-weight: 300;
+    padding: 12px 20vw;
+  }
+</style>
